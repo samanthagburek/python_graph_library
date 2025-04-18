ac884b8178130ee1f9ec485c175d3dfb84d42d07
Joe Braha <jbraha@nyu.edu>
2025-04-18 16:02:55 -0400

    basic interface definition

--- src/booty_boost/__init__.py
+++ src/booty_boost/__init__.py
@@ -1,3 +1,17 @@
 from ._booty_boost import Graph
 
 __all__ = ["Graph"]
+
+
+class Graph:
+    def __init__(self) -> None:
+        pass
+
+    def add_edge(self, node1: str, node2: str):
+        pass
+
+    def bfs(self, node: str):
+        pass
+
+    def dfs(self, node: str):
+        pass
