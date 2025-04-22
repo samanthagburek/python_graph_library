48fcfb3f14617195d1ec67af860c6582c8612deb
123R3N321 <jr5887@nyu.edu>
2025-04-22 00:15:07 -0400

    Complete is_tree() implementation.

--- src/booty_boost/graph.py
+++ src/booty_boost/graph.py
@@ -6,6 +6,9 @@ class Graph(BaseGraph):
     def __init__(self) -> None:
         self.g = _Graph()
 
+    def add_node(self, node: str):
+        return self.g.add_node(node)
+
     def add_edge(self, node1: str, node2: str):
         return self.g.add_edge(node1, node2)
 
@@ -14,3 +17,6 @@ class Graph(BaseGraph):
 
     def dfs(self, node: str):
         return self.g.dfs(node)
+
+    def is_tree(self):
+        return self.g.is_tree()
---
+++ test.py
@@ -0,0 +1,3 @@
+import booty_boost as bb
+G = bb.Graph()
+print(dir(G))  # This should print a list of methods including 'is_tree'
